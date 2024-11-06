855d6fe2f84ac2650a849fb889b0af0805c919e8
Nnasha Adama <beautyhope885@gmail.com>
2024-11-06 21:13:55 +0100

    Updated styles.css

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const colorState= document.getElementById('color-state');
 const inputField = document.getElementById('input-box');
 const listContainer= document.getElementById('inlist');
 const listNumberContainer= document.getElementById('number');
+const clearCompleted= document.getElementById('clear-completed');
+const background= document.getElementById('background');
 
 var listNumber= 0;
 listNumberContainer.innerText = String(listNumber);
@@ -34,12 +36,20 @@ function addToList(){
     
     inputField.value= '';
 
-    countList();
+    addCountList();
 }
 
-function countList(){
+function addCountList(){
     listNumber += 1;
     listNumberContainer.innerText = String(listNumber);
+
+}
+function minusCountList(){
+    if (listNumber>0){
+        listNumber -= 1;
+        listNumberContainer.innerText = String(listNumber);
+
+    }
 
 }
 
@@ -53,8 +63,45 @@ function countList(){
 listContainer.addEventListener('click', (e)=>{
     if (e.target.tagName === 'P'){
         e.target.classList.toggle('checked');
+        if (e.target.classList.contains('checked')){
+            minusCountList();
+        } else {
+            addCountList();
+        }
     } 
     if (e.target.tagName === 'DIV'){
         e.target.parentElement.remove();
+        if (!p.classList.contains('checked')){
+            minusCountList();
+        }
     }
 })
+
+
+// Clear completed todos
+
+clearCompleted.addEventListener('click', ()=>{
+        let v= listContainer.getElementsByClassName('checked');
+        for (var i=0; i< v.length; i++){
+            console.log(i);
+            i.parentElement.remove();
+
+        }
+})
+
+// Light and Dark Modes
+
+function backgroundTheme(theme,icon){
+    background.src= `images/bg-desktop-${theme}.jpg`
+    colorState.src= `images/icon-${icon}.svg`
+}
+function lightMode(){
+    backgroundTheme('light','sun');
+
+}
+
+function darkMode(){
+    backgroundTheme('dark','moon')
+}
+
+darkMode();
